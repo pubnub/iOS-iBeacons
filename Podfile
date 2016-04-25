fd410d1a29499163a67720b52be523a5c7fe4d26
Jordan Zucker <jordan.zucker@gmail.com>
2016-04-25 11:00:07 -0700

    Basic project, needs to be filled out

---
+++ Podfile
@@ -0,0 +1,9 @@
+source 'https://github.com/CocoaPods/Specs.git'
+workspace 'iBeacons.xcworkspace'
+xcodeproj 'Swift/iBeacon-Swift/iBeacon-Swift.xcodeproj'
+use_frameworks!
+
+target 'iBeacon-Swift', :exclusive => true do
+  platform :ios, '7.0'
+  pod "PubNub", "~>4"
+end
---
+++ Swift/iBeacon-Swift/iBeacon-Swift/SecondViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SecondViewController.swift
+//  iBeacon-Swift
+//
+//  Created by Jordan Zucker on 4/25/16.
+//  Copyright © 2016 Jordan Zucker. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var iBeaconSwitch: UISwitch?
+    
+    @IBAction func tappedIBeaconSwitch(sender: UIButton) {
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
